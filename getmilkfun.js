33dc60593131b783f6b27056c7b4590ec2cc5e22
KAUNGPYAEPAING <88584127+KAUNGPYAEPAING@users.noreply.github.com>
2021-08-19 18:15:11 +0630

    Create getmilkfun.js

---
+++ getmilkfun.js
@@ -0,0 +1,24 @@
+
+function BuyMilk(cash) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("Buy " + Math.floor(cash/1.5) + " bottles");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+Buymilk(5);
